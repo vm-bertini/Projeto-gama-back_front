a6f8cc23589e2a42a35cf12d9e1bb7309351255a
Victor Mario Bertini <victor.m.bertini@gmail.com>
2021-08-13 15:50:26 -0300

    Variáveis de ambiente no servidor

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: 'mysql',
-  host: 'us-cdbr-east-04.cleardb.com',
-  username: 'bd4b678b87a414',
-  password: '04b54bd5',
-  database: 'heroku_a48c8a6fe5b0fde',
+  dialect: process.env.DIALECT,
+  host: process.env.HOST,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   define: {
     timestamps: true,
     underscored: true,
